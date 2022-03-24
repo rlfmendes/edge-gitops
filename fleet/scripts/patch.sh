026185e8be01f6c949f513ef348bc0e9cbf057c5
bartr <bartr@microsoft.com>
2022-03-24 17:31:13 +0000

    removed cli

--- fleet/scripts/patch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "$(date +'%Y-%m-%d %H:%M:%S')  patched" >> /home/akdc/status
-
-# pull the latest docker images
-docker pull ghcr.io/cse-labs/webv-red:latest
-docker pull ghcr.io/cse-labs/webv-red:beta
-
-echo "$(hostname) patched"
