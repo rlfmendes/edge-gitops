87d645f5e04d3d4b55455119bae3c253176e7a4d
bartr <bartr@microsoft.com>
2022-04-11 22:12:06 +0000

    updated from main

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -8,6 +8,14 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
 # update the cli
 git -C ../cli pull
 
+# update the branch
+if [ "$(git branch --show-current)" != "main" ]
+then
+    echo "Updating Codespaces"
+    git restore -s origin/main README.md
+    git restore -s origin/main .devcontainer
+fi
+
 # update the base docker images
 docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine
 docker pull mcr.microsoft.com/dotnet/aspnet:5.0-alpine
